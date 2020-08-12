581c4be10d91a10aebafef4031e4b194beac57af
gaoey <gaoey@hotmail.com>
2020-08-12 22:33:13 +0700

    add icon in navigation

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { LanguageProvider } from './context/language/LanguageContext';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import * as Screen from './screens'
 
 const Stack = createStackNavigator();
@@ -12,9 +13,9 @@ const Tab = createBottomTabNavigator();
 function Main() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="HomeScreen" component={Screen.Main.HomeScreen} options={{ headerShown: true }} />
-      <Tab.Screen name="SecondScreen" component={Screen.Main.SecondScreen} options={{ headerShown: true }} />
-      <Tab.Screen name="ThirdScreen" component={Screen.Main.ThirdScreen} options={{ headerShown: true }} />
+      <Tab.Screen name="HomeScreen" component={Screen.Main.HomeScreen} options={{ tabBarIcon: () => <Icon name="chevron-down" size={30} color="grey" /> }} />
+      <Tab.Screen name="SecondScreen" component={Screen.Main.SecondScreen} options={{ tabBarIcon: () => <Icon name="chevron-down" size={30} color="grey" /> }} />
+      <Tab.Screen name="ThirdScreen" component={Screen.Main.ThirdScreen} options={{ tabBarIcon: () => <Icon name="chevron-down" size={30} color="grey" /> }} />
     </Tab.Navigator>
   );
 }
