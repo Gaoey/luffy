7814db17678c061ced7e1b661724fbe22250169a
gaoey <gaoey@hotmail.com>
2020-08-12 21:41:15 +0700

    add headline

--- screens/authorization/LoginScreen.js
+++ screens/authorization/LoginScreen.js
@@ -1,31 +1,44 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 import BG from '../../assets/bg-default.jpg'
+import { Formik } from 'formik';
 import { PageTemplate, Space } from '../../components'
-
-import { TextInput, Card, Title, Paragraph, Button } from 'react-native-paper';
+import { TextInput, Card, Headline, Paragraph, Button } from 'react-native-paper';
 
 export default function LoginScreen({ navigation }) {
-  const [email, setEmail] = React.useState('');
-  const [pwd, setPwd] = React.useState('');
   return (
     <PageTemplate backgroundImage={BG}>
+      <Headline style={{ fontWeight: "bold", color: "white", textAlign: "center" }}>LUFFY STARTER KIT</Headline>
+      <Space />
       <Card>
         <Card.Content>
-          <TextInput
-            label="Email"
-            value={email}
-            onChangeText={text => setEmail(text)}
-          />
-          <Space />
-          <TextInput
-            label="Password"
-            value={pwd}
-            secureTextEntry
-            onChangeText={text => setPwd(text)}
-          />
-          <Space />
-          <Button icon="login" mode="contained" onPress={() => navigation.navigate("HomeScreen")}>Sign In</Button>
+          <Formik
+            initialValues={{ email: '', password: '' }}
+            onSubmit={values => {
+              const { email, password } = values;
+              // login(email, password)
+              navigation.navigate("HomeScreen");
+            }}
+          >
+            {({ handleSubmit, handleChange, values }) => (
+              <View>
+                <TextInput
+                  label="Email"
+                  value={values.email}
+                  onChangeText={handleChange('email')}
+                />
+                <Space />
+                <TextInput
+                  label="Password"
+                  value={values.password}
+                  onChangeText={handleChange('password')}
+                  secureTextEntry
+                />
+                <Space />
+                <Button icon="login" mode="contained" onPress={handleSubmit}>Sign In</Button>
+              </View>
+            )}
+          </Formik>
         </Card.Content>
       </Card>
     </PageTemplate >
